31b190467c95002c80a870f32bbba67b5a377826
jeonsangmin <jsangmin99@gmail.com>
2022-07-23 16:15:31 +0900

    Revert "2022-07-21"
    This reverts commit a7ef823f9dea40ea06769626313fbfc226b34f57.
    
    # Conflicts:
    #	src/main/java/com/springbook/biz/common/BeforeAdvice.java
    #	src/main/java/com/springbook/biz/common/LogAdvice.java
    #	src/main/resources/web/WEB-INF/applicationContext.xml

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,21 +1,9 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Service;
 
-@Service
-@Aspect
 public class LogAdvice {
-    @Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
-    public void allPointcut(){}
-    @Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
-    public void getPointcut(){}
-
-    @Before("allPointcut()")
-    public void printLog(){
+    public void printLog(Joinpoint jp){
         System.out.println("[공통 로그] 비즈니스 로직 수행전 동작");
     }
 }
